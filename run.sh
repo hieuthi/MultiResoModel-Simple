2a3761217f6bc6bae77bbc1cc2b985da26d2aa01
Luong Hieu Thi <luonghieuthi@gmail.com>
2025-03-20 16:20:34 +0800

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -18,25 +18,25 @@ if [ ! -f downloads/database_dev.tar.gz ]; then wget https://zenodo.org/api/reco
 
 echo "$0: extract data to PartialSpoof/"
 mkdir -p PartialSpoof/wav
-if [ ! -d PartialSpoof/wav/eval ]; then tar -xvf downloads/database_eval.tar.gz && mv downloads/database/eval/con_wav PartialSpoof/wav/eval; fi
-if [ ! -d PartialSpoof/wav/train ]; then tar -xvf downloads/database_train.tar.gz && mv downloads/database/train/con_wav PartialSpoof/wav/train; fi
-if [ ! -d PartialSpoof/wav/dev ]; then tar -xvf downloads/database_dev.tar.gz && mv downloads/database/dev/con_wav PartialSpoof/wav/dev; fi
+if [ ! -d PartialSpoof/wav/eval ]; then tar -xvf downloads/database_eval.tar.gz -C downloads/ && mv downloads/database/eval/con_wav PartialSpoof/wav/eval; fi
+if [ ! -d PartialSpoof/wav/train ]; then tar -xvf downloads/database_train.tar.gz -C downloads/ && mv downloads/database/train/con_wav PartialSpoof/wav/train; fi
+if [ ! -d PartialSpoof/wav/dev ]; then tar -xvf downloads/database_dev.tar.gz -C downloads/ && mv downloads/database/dev/con_wav PartialSpoof/wav/dev; fi
 
 echo "$0: download labels for PartialSpoof"
-if [ ! -d PartialSpoof/label_PartialSpoof_dev.txt ]; then wget https://github.com/hieuthi/MultiResoModel-Simple/releases/download/v0.1.0/label_PartialSpoof_dev.txt -P PartialSpoof; fi
-if [ ! -d PartialSpoof/label_PartialSpoof_train.txt ]; then wget https://github.com/hieuthi/MultiResoModel-Simple/releases/download/v0.1.0/label_PartialSpoof_train.txt -P PartialSpoof; fi
-if [ ! -d PartialSpoof/label_PartialSpoof_eval.txt ]; then wget https://github.com/hieuthi/MultiResoModel-Simple/releases/download/v0.1.0/label_PartialSpoof_eval.txt -P PartialSpoof; fi
+if [ ! -f PartialSpoof/label_PartialSpoof_dev.txt ]; then wget https://github.com/hieuthi/MultiResoModel-Simple/releases/download/v0.1.0/label_PartialSpoof_dev.txt -P PartialSpoof; fi
+if [ ! -f PartialSpoof/label_PartialSpoof_train.txt ]; then wget https://github.com/hieuthi/MultiResoModel-Simple/releases/download/v0.1.0/label_PartialSpoof_train.txt -P PartialSpoof; fi
+if [ ! -f PartialSpoof/label_PartialSpoof_eval.txt ]; then wget https://github.com/hieuthi/MultiResoModel-Simple/releases/download/v0.1.0/label_PartialSpoof_eval.txt -P PartialSpoof; fi
 
 echo "$0: prepare scp files"
 mkdir -p PartialSpoof/scp
-if [ ! -d PartialSpoof/scp/wav-train.scp ]; then find PartialSpoof/wav/train -name "*.wav" | sort > PartialSpoof/scp/wav-train.scp; fi
-if [ ! -d PartialSpoof/scp/wav-dev.scp ]; then find PartialSpoof/wav/dev -name "*.wav" | sort > PartialSpoof/scp/wav-dev.scp; fi
-if [ ! -d PartialSpoof/scp/wav-eval.scp ]; then find PartialSpoof/wav/eval -name "*.wav" | sort > PartialSpoof/scp/wav-eval.scp; fi
+if [ ! -f PartialSpoof/scp/wav-train.scp ]; then find PartialSpoof/wav/train -name "*.wav" | sort > PartialSpoof/scp/wav-train.scp; fi
+if [ ! -f PartialSpoof/scp/wav-dev.scp ]; then find PartialSpoof/wav/dev -name "*.wav" | sort > PartialSpoof/scp/wav-dev.scp; fi
+if [ ! -f PartialSpoof/scp/wav-eval.scp ]; then find PartialSpoof/wav/eval -name "*.wav" | sort > PartialSpoof/scp/wav-eval.scp; fi
 
 
 ## Training detection model with PartialSpoof
 echo "$0: train the multiresomodel with PartialSpoof"
-python -u train.py --config configs/baseline.toml --batch_size 8 --num_workers 6 > logs/baseline.log 2>&1 &
+python -u train.py --config configs/baseline.toml --batch_size 8 --num_workers 6 > logs/baseline.log 2>&1
 
 echo "$0: the training is finished edit the script if you want to run evaluation"
 # ## Evaluating with PartialSpoof eval set
